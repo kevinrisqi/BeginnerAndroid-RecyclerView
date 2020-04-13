03fdb56ac3ccf93a3964ff8091b0b853547c7069
Kevin <kevinrisqi22@gmail.com>
2020-04-13 14:59:57 +0700

    Add menu

--- app/src/main/java/com/zenai/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/zenai/myrecyclerview/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.zenai.myrecyclerview.adapter.ListHeroAdapter;
 import com.zenai.myrecyclerview.model.Hero;
@@ -28,6 +31,31 @@ public class MainActivity extends AppCompatActivity {
 
         list.addAll(HeroesData.getListData());
         showRecyclerList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+    public void setMode(int selectedMode) {
+        switch (selectedMode) {
+            case R.id.action_list:
+                Toast.makeText(this, "List is Open !", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_grid:
+                Toast.makeText(this, "Grid is Open !", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_cardview:
+                Toast.makeText(this, "CardView is Open !", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     private void showRecyclerList(){
