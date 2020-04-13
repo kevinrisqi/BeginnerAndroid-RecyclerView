f320a6c3cf8dbf6e8604e4de91fc7073dbfdd880
Kevin <kevinrisqi22@gmail.com>
2020-04-13 13:05:39 +0700

    Make Mode List with RecyclerView

---
+++ app/src/main/java/com/zenai/myrecyclerview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.zenai.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.zenai.myrecyclerview.adapter.ListHeroAdapter;
+import com.zenai.myrecyclerview.model.Hero;
+import com.zenai.myrecyclerview.model.HeroesData;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter= new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+}
